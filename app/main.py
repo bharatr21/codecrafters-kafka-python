c8645d646031e6a4e69c5763e4c1a6498fff8280
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-12-26 13:27:00 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 import socket  # noqa: F401
 
 
+def create_message(id):
+    id_bytes = id.to_bytes(4, "big")
+    return len(id_bytes).to_bytes(4, "big") + id_bytes
+
+def handle_client(client):
+    data = client.recv(1024)
+    print(f"Received data: {data}")
+    client.sendall(create_message(7))
+    client.close()
+
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     server.listen()
 
     while True:
-        client, addr = server.accept()
-        print(f"Connection from {addr}")
-        client.close()
+        client, client_address = server.accept()
+        print(f"Received connection from {client_address}")
+        handle_client(client)
 
 
 if __name__ == "__main__":
